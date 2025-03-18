a7dae25ce3b674a1e755a81e7c59c36ef0124369
Ethan <757819951@qq.com>
2025-03-18 17:18:59 +0800

    procurement p

--- apps/products/views.py
+++ apps/products/views.py
@@ -136,10 +136,7 @@ class ProductViewSet(viewsets.ModelViewSet):
                 )
 
             # 使用原始文件名保存到products目录
-            filename = f"products/{image_file.name}"
-            
-            # 保存文件
-            path = default_storage.save(filename, ContentFile(image_file.read()))
+            path = default_storage.save(os.path.join('products', image_file.name), ContentFile(image_file.read()))
             print(path) 
             image_url = default_storage.url(path)
             print(image_url)    
